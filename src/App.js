5af25be59a28eee9071735bf273727213fad70c7
LucasPerno <lucas.perno@hotmail.com>
2022-09-27 12:05:44 -0300

    Agregado de icono Cart e ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import {NavBar} from "./components/index"
+import {ItemListContainer} from "./components/index"
 import { HomeScreen } from './views';
 import './components/NavBar/index.css';
 
+
 function App() {
   return (
+    <div className = "app">
     <NavBar></NavBar>
+    <ItemListContainer></ItemListContainer>
+    </div>
   );
 }
 
---
+++ src/components/ItemListContainer/itemListContainer.js
@@ -0,0 +1,11 @@
+const ItemListContainer = () => {
+  return (
+    <main>
+      <div className = "">
+        <h1>Contenido del main</h1>
+      </div>
+    </main>
+  )
+}
+
+export default ItemListContainer
